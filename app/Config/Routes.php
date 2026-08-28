0c9ff0cc7592656f30f9738b500eff1693d886e8
NasaGV <gvillanuevah@miumg.edu.gt>
2026-08-28 16:32:26 -0600

    feat(lecturas): agregar rutas del modulo de lecturas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,4 +10,9 @@ $routes->get('/logout', 'Auth::logout');
 
 $routes->get('/dashboard', 'Dashboard::index', ['filter' => 'auth']);
 
+// Rutas del modulo de Lecturas + Recibo
+$routes->get('/lecturas/nueva/(:num)', 'Lecturas::nueva/$1', ['filter' => 'auth']);
+$routes->post('/lecturas/guardar', 'Lecturas::guardar', ['filter' => 'auth']);
+$routes->get('/lecturas/recibo/(:num)', 'Lecturas::recibo/$1', ['filter' => 'auth']);
+
 $routes->get('/', 'Home::index');
